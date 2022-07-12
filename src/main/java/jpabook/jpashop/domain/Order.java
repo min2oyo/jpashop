5803c690b491398418d4d0de610ed11ed38618a2
min2oyo <min2u@outlook.com>
2022-07-12 14:14:19 +0900

    주문 도메인 개발 | 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -33,12 +33,14 @@ public class Order {
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime localDateTime;    // 주문시간
+    private LocalDateTime orderDate;    // 주문시간
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
-    // 연관관계 Method
+
+    /*** 연관관계 Method ***/
+
     public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
@@ -53,5 +55,49 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+
+    /*** 생성 메서드 ***/
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {    // set이 아닌, 생성메서드로 완결
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+
+    /*** 비즈니스 로직 ***/
+    // 주문 취소
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {  // 배송 완료
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다. ");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+
+    /*** 조회 로직 ***/
+
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {    // for 앞에 커서 - Alt + Enter - Replace With Sum: 대박 축소
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
+    
+
 
 }
